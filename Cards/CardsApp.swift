3f89d066fdb04431daae915ba4878831498cd5ba
Jake Abramson <abramso@bc.edu>
2026-02-22 15:19:03 -0500

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Jake Abramson on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
